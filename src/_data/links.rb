bbfc567ed916d40d7925c1c6ad5d83632fc0cf77
atrius <jake14.d@gmail.com>
2023-02-25 22:15:23 -0700

    footer

--- src/_data/links.rb
+++ src/_data/links.rb
@@ -1,3 +1,5 @@
 {
-  twitter: "https://twitter.com/nuckdev"
+  github: "https://github.com/jake-duchesne",
+  twitter: "https://twitter.com/nuckdev",
+  instagram: "https://instagram.com/ohc_nada"
 }
