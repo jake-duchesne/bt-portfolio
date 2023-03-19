42e56425eda89e606a2ba65699b62419c21e8b8c
atrius <jake14.d@gmail.com>
2023-03-18 18:43:27 -0700

    hideable history, tech section for work, add logo

--- src/_components/work_card.rb
+++ src/_components/work_card.rb
@@ -3,7 +3,7 @@ class WorkCard < BaseComponent
   renders_one :content_section
   renders_one :tech
 
-  def initialize(company:, location:, role:, start_date:, end_date: nil, current: false)
-    @company, @location, @role, @start_date, @end_date, @current = company, location, role, start_date, end_date, current
+  def initialize(company:, location:, role:, start_date:, end_date: nil, current: false, image: nil)
+    @company, @location, @role, @start_date, @end_date, @current, @image = company, location, role, start_date, end_date, current, image
   end
 end
